ece5637115c7b545c2483c97282ba90bbb14efdf
Mausam Baduwal <mausambaduwal303@gmail.com>
2024-08-02 17:31:24 +0545

    Ready to work on frontend

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,9 @@ const port = 5000
 
 app.use(express.json())
 //Available Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
